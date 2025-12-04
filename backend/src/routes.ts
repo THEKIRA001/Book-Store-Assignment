64bbf98c23d0d2477a77212ab829cf960f7d470b
Shubham Bolave <111694017+THEKIRA001@users.noreply.github.com>
2025-12-04 16:12:45 +0530

    Commit 3 : Implemented & Tested Get Books API

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,13 @@
 import {Router, Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
 import { authenticateToken, AuthRequest } from './middleware/auth';
+import path from 'path';
+import fs from 'fs';
 
 const router = Router();
 const secret = process.env.JWT_SECRET || 'defaultsecret';
 
-// Login
+// Login API Route 
 router.post('/login', (req: Request, res: Response) => {
    const { username, password } = req.body;
    if(username === 'admin' && password === 'admin') {
@@ -17,12 +19,28 @@ router.post('/login', (req: Request, res: Response) => {
    }
 });
 
-// Protected
+// Protected API Route
 router.get('/protected', authenticateToken, (req: AuthRequest, res: Response) => {
     res.json({ message: 'This is protected data.', user: req.user });
 });
 
 
+// Simulating Books from DB in JSON format
+const getBooksFromDB = () => {
+    const filepath = path.join(__dirname, 'data', 'books.json');
+    const data = fs.readFileSync(filepath, 'utf-8');
+    return JSON.parse(data);
+}
+
+// Get Books API Route (Public, No authentication Required)
+router.get('/books', (req: AuthRequest, res: Response) => {
+    try{
+        const books = getBooksFromDB();
+    res.json(books);
+    } catch (error) {
+        res.status(500).json({ message: 'Error reading Database'});
+    }
+});
 
 
 export default router;
